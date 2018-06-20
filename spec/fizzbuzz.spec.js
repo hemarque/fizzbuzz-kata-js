a7e23547aa1d799408df8015a4f7021bb762fa0b
Helder De Oliveira <helderdeoliveira@Helder.local>
2018-06-20 13:51:08 +0200

    fizzbuzz of 2

--- spec/fizzbuzz.spec.js
+++ spec/fizzbuzz.spec.js
@@ -1,8 +1,11 @@
 function fizzbuzz(number){
-	return 1; 
+	return number; 
 }
 describe("fizzbuzz", function(){
 	it("fizzbuzz of 1", function(){
 		expect(fizzbuzz(1)).toBe(1);
+	});
+	it("fizzbuzz of 2", function(){
+		expect(fizzbuzz(2)).toBe(2);
 	});
 }); 
