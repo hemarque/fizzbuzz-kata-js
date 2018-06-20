11cf1ff593a0a6e8c71fa712dd9635a966be42a8
Helder De Oliveira <helderdeoliveira@Helder.local>
2018-06-20 13:49:41 +0200

    fizzbuzz of 1

---
+++ readme.md
@@ -0,0 +1,3 @@
+npm install -g jasmine
+jasmine init
+jasmine-node --autotest spec/
---
+++ spec/fizzbuzz.spec.js
@@ -0,0 +1,8 @@
+function fizzbuzz(number){
+	return 1; 
+}
+describe("fizzbuzz", function(){
+	it("fizzbuzz of 1", function(){
+		expect(fizzbuzz(1)).toBe(1);
+	});
+}); 
